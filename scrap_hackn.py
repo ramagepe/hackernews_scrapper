815c6d5b2b4d878c552a73772915d853cbfab626
ramagepe <ramagarciapedrosa@gmail.com>
2022-04-27 20:05:00 -0300

    micro refactor

--- scrap_hackn.py
+++ scrap_hackn.py
@@ -19,18 +19,22 @@ def filter_news(response):
     scores = clean_scores(soup.select('.score'))
     return package_data(titles, links, scores)
 
+
 def clean_titles(titles):
     """Cleans scrapped post titles"""
     return [title.string for title in titles]
+
 
 def clean_links(links):
     """Cleans scrapped post links"""
     return [link['href'] for link in links]
 
+
 def clean_scores(scores):
     """Cleans scrapped post scores"""
     return [int(score.string[:score.string.find(' ')])
-                 for score in scores]
+            for score in scores]
+
 
 def package_data(titles, links, scores):
     """Recieves 3 cleaned types of data and chunk them into a curated list
@@ -45,19 +49,23 @@ def package_data(titles, links, scores):
     """
     floor = 99
     curated_lst = []
-    for i, score in enumerate(scores):
+    for title, link, score in zip(titles, links, scores):
         if score > floor:
-            curated_lst.append({'title': titles[i], 'link': links[i], 'votes': score})
+            curated_lst.append({'title': title, 'link': link, 'votes': score})
     return curated_lst
+
 
 def sort_data(packaged_data):
     """Sorts the curated data from higher to lower votes"""
     return sorted(packaged_data, key=lambda x: x['votes'], reverse=True)
 
+
 def log_scrapping(current_post):
     """Logs in terminal the posts being open by the browser"""
     title, votes = current_post['title'], current_post['votes']
-    printr(f'[bold white]>>[/bold white] [bold blue]{title}[/bold blue] --> {votes} votes')
+    printr(
+        f'[bold white]>>[/bold white] [bold blue]{title}[/bold blue] --> {votes} votes')
+
 
 def page_by_page():
     """Wrapper function that allows to scrap page by page"""
@@ -74,7 +82,8 @@ def page_by_page():
             log_scrapping(post)
         page_num += 1
 
-def many_sorted(num_posts:int, last_page:int):
+
+def many_sorted(num_posts: int, last_page: int):
     """Wrapper function that shows a given amount of posts from a given amount of pages
 
     Args:
@@ -84,21 +93,25 @@ def many_sorted(num_posts:int, last_page:int):
     current_page = 1
     data_list = []
     while current_page <= last_page:
-        res = requests.get(f'https://news.ycombinator.com/news?p={current_page}')
+        res = requests.get(
+            f'https://news.ycombinator.com/news?p={current_page}')
         data_list += filter_news(res)
         current_page += 1
     data_list = sort_data(data_list)
-    for i in range(1, num_posts+1):
+    for i in range(num_posts+1):
         webbrowser.open(data_list[i]['link'])
         log_scrapping(data_list[i])
+
 
 def log_error():
     """Logs message into the console in case an error occurs"""
     print('\n>> ERROR: Invalid flags')
     print('\n\tall --> shows page by page')
-    print('\tbest [num of posts] [num of pages] --> shows top [num of posts] from [num of pages] pages')
+    print(
+        '\tbest [num of posts] [num of pages] --> shows top [num of posts] from [num of pages] pages')
     print('\n\tExample 1 >> python scrap_hackn.py all')
     print('\tExample 2 >> python scrap_hackn.py best 10 5')
+
 
 if __name__ == '__main__':
     try:
